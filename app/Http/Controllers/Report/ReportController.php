27d61862c663fe51ffe590d54b5c5af96ba2aeb0
edwin <emanuel@coboser.com>
2016-02-02 09:14:07 -0400

    edwin: add validacion reportes generales

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -9,12 +9,15 @@ use Sibas\Http\Requests;
 use Sibas\Http\Controllers\Controller;
 
 class ReportController extends Controller {
-
+    var $object = [];
+    public $value = '';
+    public $valueOr = '';
     public function __construct() {
         $this->users = DB::table('ad_users')->lists('username', 'id');
         $this->agencies = DB::table('ad_agencies')->lists('name', 'id');
         $this->cities = DB::table('ad_cities')->lists('name', 'id');
         $this->extencion = DB::table('ad_cities')->lists('name', 'abbreviation');
+        $this->observation = DB::table('op_de_observations')->orderBy('id','DESC')->get();
     }
 
     /**
@@ -38,13 +41,45 @@ class ReportController extends Controller {
                 ->join('ad_users', 'op_de_headers.ad_user_id', '=', 'ad_users.id')
                 ->join('ad_coverages', 'op_de_headers.ad_coverage_id', '=', 'ad_coverages.id')
                 ->select('op_de_headers.policy_number', 'op_de_headers.id', 'ad_coverages.name', 'op_de_headers.operation_number', 'op_de_headers.amount_requested', 'op_de_headers.currency', 'op_de_headers.term', 'op_de_headers.type_term', 'op_de_headers.total_rate', 'op_de_headers.total_premium', 'op_de_headers.date_issue', 'ad_users.username', 'ad_users.full_name')
-                ->where('op_de_headers.issued', 1)
+                //edw-->->where('op_de_headers.issued', 1)
                 ->where('op_de_headers.type', 'I');
         $details = array();
         $result = array();
         $flagClient = 0;
+        
         if ($request->get('_token')) {
-
+            # relacion facultativos
+            if ($request->get('rechazado') || $request->get('no_freecover') || $request->get('extraprima') || $request->get('no_extraprima') || $request->get('pendiente') || $request->get('subsanado') || $request->get('observado')){
+                $query->join('op_de_details', 'op_de_headers.id', '=', 'op_de_details.op_de_header_id');
+                $query->leftJoin('op_de_facultatives', 'op_de_facultatives.op_de_detail_id', '=', 'op_de_details.id');
+            }
+            
+            $arr = $this->role($request);
+            
+            foreach ($arr as $key => $value) {
+                
+                if ($key == 'and') {
+                    $this->value = $value;
+                    $query->where(function($q) {
+                        foreach ($this->value as $key2 => $value2) {
+                            $q->whereRaw($key2 . '="'.$value2.'"');
+                        }
+                        $q->whereRaw('`op_de_headers`.`type`="I"');
+                    });
+                }elseif ($key == 'or'){
+                    foreach ($value as $key => $value) {
+                        $this->valueOr = $value;
+                        $query->orWhere(function($q) {
+                            foreach ($this->valueOr as $key2 => $value2) {
+                                $q->whereRaw($key2 . '="'.$value2.'"');
+                            }
+                            $q->whereRaw('`op_de_headers`.`type`="I"');
+                        });
+                    }
+                }
+                
+            }
+            
             # numero poliza
             if ($request->get('numero_poliza'))
                 $query->where('op_de_headers.policy_number', $request->get('numero_poliza'));
@@ -101,6 +136,9 @@ class ReportController extends Controller {
             }
             $result = $var;
         }
+        
+        $result = $this->observations($request, $result);
+        
 
         # validacion exporta xls
         if ($request->get('xls_download'))
@@ -109,6 +147,137 @@ class ReportController extends Controller {
         return view('report.general', compact('result', 'users', 'agencies', 'cities', 'extencion', 'valueForm'));
     }
     
+    /**
+     * regla de consulta para el filtro
+     * @param type $request
+     * @return array
+     */
+    public function role($request) {
+        $consult = [];
+        if ($request->get('freecover'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1, '`op_de_headers`.`facultative`' => 0);
+
+        # no freecover
+        if ($request->get('no_freecover'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1, '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`state`' => 'PR', '`op_de_facultatives`.`approved`' => 1);
+
+        # emitido
+        if ($request->get('emitido'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1);
+
+        # no emitido
+        if ($request->get('no_emitido'))
+            $consult[] = array('`op_de_headers`.`issued`' => 0);
+        
+        # extraprima
+        if ($request->get('extraprima'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1, '`op_de_facultatives`.`state`' => 'PR', '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`surcharge`' => 1);
+
+        # no extraprima
+        if ($request->get('no_extraprima'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1, '`op_de_facultatives`.`state`' => 'PR', '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`surcharge`' => 0);
+
+        # rechazados
+        if ($request->get('rechazado'))
+            $consult[] = array('`op_de_facultatives`.`approved`' => 0, '`op_de_facultatives`.`state`' => 'PR');
+
+        # polizas anuladas
+        if ($request->get('anulado'))
+            $consult[] = array('`op_de_headers`.`issued`' => 1, '`op_de_headers`.`canceled`' => 0);
+
+        # pendiente
+        if ($request->get('pendiente'))
+            $consult[] = array('`op_de_headers`.`issued`' => 0, '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`state`' => 'PE');
+        
+        # subsanado
+        if ($request->get('subsanado'))
+            $consult[] = array('`op_de_headers`.`issued`' => 0, '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`state`' => 'PE');
+        
+        # observado
+        if ($request->get('observado'))
+            $consult[] = array('`op_de_headers`.`issued`' => 0, '`op_de_headers`.`facultative`' => 1, '`op_de_facultatives`.`state`' => 'PE');
+        
+        $arr=[];
+        foreach ($consult as $key => $value) {
+            if($key == 0){
+                $arr['and']=$value;
+            }else{
+                $arr['or'][]=$value;
+            }
+        }
+        return $arr;
+                
+    }
+    
+    /**
+     * regla de consulta observacion para el filtro
+     * @param type $request
+     * @return array
+     */
+    public function observations($request, $array) {
+        # registros observaciones
+        $opDeObservations = DB::table('op_de_observations')->orderBy('id','desc')->get();
+        $observation = [];
+        foreach ($opDeObservations as $key => $value) {
+            if(!isset($observation[$value->op_de_facultative_id])){
+                $observation[$value->op_de_facultative_id] =  $value;       
+            }
+        }
+        
+        # estados
+        $adStates = DB::table('ad_states')->get();
+        $states = [];
+        foreach ($adStates as $key => $value) {
+            $states[$value->id] = $value;
+        }
+        
+        $consult = [];
+        if ($request->get('pendiente'))
+            $consult[] = 'no';
+        
+        if ($request->get('subsanado')){
+            $consult[] = 'si';
+            $consult[] = 'cl';
+        }
+        
+        if ($request->get('subsanado'))
+            $consult[] = 'si';
+        
+        if(count($consult)>0){
+            $ress = [];
+            foreach ($array as $key => $value) {
+                if (in_array('no', $consult) && !in_array('si', $consult)) {
+                    # no debe existir en observaciones
+                    if(!isset($observation['1453731639'])){
+                            $ress[$key] = $value;
+                        }
+                } elseif (in_array('no', $consult) && in_array('si', $consult)) {
+                    #exista o no ingresan todos
+                    $ress[$key] = $value;
+                    
+                } elseif (in_array('si', $consult) && !in_array('no', $consult)) {
+                    
+                    if (in_array('cl', $consult)) {
+                        if(isset($observation['1453731639']) && $states[$observation['1453731639']->ad_state_id]->slug == 'cl'){
+                            $ress[$key] = $value;
+                        }
+                    } else {
+                        #solo si existe
+                        if(isset($observation['1453731639'])){
+                            $ress[$key] = $value;
+                        }
+                    }
+                    
+                }
+            }
+            return $ress;
+        }else{
+            return $array;
+        }
+    }
+
+   
+
     /**
      * fucion retorna cotizaciones
      * @param \Illuminate\Http\Request $request
@@ -246,6 +415,17 @@ class ReportController extends Controller {
             'sucursal' => ($request->get('sucursal')) ? $request->get('sucursal') : '',
             'fecha_ini' => ($request->get('fecha_ini')) ? $request->get('fecha_ini') : '',
             'fecha_fin' => ($request->get('fecha_fin')) ? $request->get('fecha_fin') : '',
+            'anulado' => ($request->get('anulado')) ? $request->get('anulado') : '',
+            'rechazado' => ($request->get('rechazado')) ? $request->get('rechazado') : '',
+            'freecover' => ($request->get('freecover')) ? $request->get('freecover') : '',
+            'no_freecover' => ($request->get('no_freecover')) ? $request->get('no_freecover') : '',
+            'extraprima' => ($request->get('extraprima')) ? $request->get('extraprima') : '',
+            'no_extraprima' => ($request->get('no_extraprima')) ? $request->get('no_extraprima') : '',
+            'emitido' => ($request->get('emitido')) ? $request->get('emitido') : '',
+            'no_emitido' => ($request->get('no_emitido')) ? $request->get('no_emitido') : '',
+            'pendiente' => ($request->get('pendiente')) ? $request->get('pendiente') : '',
+            'subsanado' => ($request->get('subsanado')) ? $request->get('subsanado') : '',
+            'observado' => ($request->get('observado')) ? $request->get('observado') : '',
         );
         return $arr;
     }
--- resources/views/report/general.blade.php
+++ resources/views/report/general.blade.php
@@ -46,9 +46,12 @@
                         <div class="panel-body ">
                             <div class="col-xs-12 col-md-3">
                                 <div class="form-group">
-                                    <label class="control-label col-lg-4">Nro. Póliza: </label>
+                                    <label class="control-label col-lg-4">Póliza: </label>
                                     <div class="col-lg-8">
-                                        <input name="numero_poliza" value="{{ $valueForm['numero_poliza'] }}" type="text" class="form-control ui-wizard-content" placeholder="Nro. Póliza">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">Nro</span>
+                                            <input name="numero_poliza" value="{{ $valueForm['numero_poliza'] }}" type="text" class="form-control ui-wizard-content" placeholder="Nro. Póliza">
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -135,15 +138,84 @@
                                 </div>
                             </div>
                             <input type="hidden" id="xls_download" name="xls_download" value="0">
-                            <div class="col-xs-12 col-md-3">
-                                <div class="text-right">
-                                    <a href="{{ route('report.report_general') }}" class="btn btn-default" title="Cancelar">Cancelar <i class="icon-cross2 position-right"></i></a>
-                                    <button type="submit" class="btn btn-primary" id="buscar" onclick="$('#xls_download').val(0);">Buscar <i class="icon-search4 position-right"></i></button>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="panel panel-flat">
+                                <div class="panel-body">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="checkbox-inline checkbox-right">
+                                                <strong>Pendiente:</strong> &nbsp;
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" class="styled" name="pendiente" value="1" id="pendiente" @if($valueForm['pendiente']==1) checked @endif>     
+                                                       Pendiente
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" class="styled" name="subsanado" value="1" id="subsanado" @if($valueForm['subsanado']==1) checked @endif>
+                                                       Subsanado/Pendiente
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" class="styled" name="observado" value="1" id="observado" @if($valueForm['observado']==1) checked @endif>
+                                                       Observado
+                                            </label>
+                                        </div>
+                                        <hr />
+                                        <div class="form-group">
+                                            <label class="checkbox-inline checkbox-right">
+                                                <strong>Aprobado:</strong> &nbsp;
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value="1" class="styled" name="freecover" id="freecover" @if($valueForm['freecover']==1) checked @endif>
+                                                       Free Cover
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value="1" class="styled" name="no_freecover" id="no_freecover" @if($valueForm['no_freecover']==1) checked @endif>
+                                                       No Free Cover
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value=1"" class="styled" name="extraprima" id="extraprima" @if($valueForm['extraprima']==1) checked @endif>
+                                                       Extraprima
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value="1" class="styled" name="no_extraprima" id="no_extraprima" @if($valueForm['no_extraprima']==1) checked @endif>
+                                                       No Extraprima
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value="1" class="styled" name="emitido" id="emitido" @if($valueForm['emitido']==1) checked @endif>
+                                                       Emitido
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" value="1" class="styled" name="no_emitido" id="no_emitido" @if($valueForm['no_emitido']==1) checked @endif>
+                                                       No Emitido
+                                            </label>
+                                        </div>
+                                        <hr />
+                                        <div class="form-group">
+                                            <label class="checkbox-inline checkbox-right">
+                                                <strong>Rechazado/Anulado:</strong> &nbsp;
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" class="styled" value="1" name="rechazado" id="rechazado" @if($valueForm['rechazado']==1) checked @endif>
+                                                       Rechazado
+                                            </label>
+                                            <label class="checkbox-inline checkbox-left">
+                                                <input type="checkbox" class="styled" value="1" name="anulado" id="anulado" @if($valueForm['anulado']==1) checked @endif>
+                                                       Anulado
+                                            </label>
+                                        </div>
+                                    </div>
+                                    
                                 </div>
                             </div>
-                            
                         </div>
-                        <hr />
+                        <div class="col-xs-12 col-md-12">
+                            <div class="text-right">
+                                <a href="{{ route('report.report_general') }}" class="btn btn-default" title="Cancelar">Cancelar <i class="icon-cross2 position-right"></i></a>
+                                <button type="submit" class="btn btn-primary" id="buscar" onclick="$('#xls_download').val(0);">Buscar <i class="icon-search4 position-right"></i></button>
+                            </div>
+                            <p>&nbsp;</p>
+                        </div>
                     {!! Form::close() !!}
                 </div>
                 <div class="col-xs-12 col-md-12">  
@@ -230,7 +302,7 @@
     </div>
 </div>
 
- 
+
 
 <!-- modal -->
 @include('partials.modal')
