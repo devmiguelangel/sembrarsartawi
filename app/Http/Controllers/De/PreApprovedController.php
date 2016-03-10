136f59f0e1d2eab2cf11742113a35650db04d292
Miguel Angel <djmiguelarango@gmail.com>
2016-03-10 16:21:26 +0000

    Cancellation, Issue and PreApproved DE refactor

--- app/Http/Controllers/De/PreApprovedController.php
+++ app/Http/Controllers/De/PreApprovedController.php
@@ -10,33 +10,38 @@ use Sibas\Repositories\De\PreApprovedRepository;
 
 class PreApprovedController extends Controller
 {
+
     use ReportTrait;
+
     /**
      * @var PreApprovedRepository
      */
     protected $repository;
+
 
     public function __construct(PreApprovedRepository $repository)
     {
         $this->repository = $repository;
     }
 
+
     /**
-     * @param Guard $auth
+     * @param Guard   $auth
      * @param Request $request
-     * @param string $rp_id
+     * @param string  $rp_id
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function lists(Guard $auth, Request $request, $rp_id)
     {
         $data    = $this->data($auth->user());
-        $headers = [];
+        $headers = [ ];
 
         if ($request->has('_token')) {
             $request->flash();
-
-            $headers = $this->repository->getHeaderList($request);
         }
+
+        $headers = $this->repository->getHeaderList($request);
 
         return view('de.pre-approved.list', compact('rp_id', 'headers', 'data'));
     }
