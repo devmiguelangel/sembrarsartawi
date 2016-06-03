5bd28eecfd1c5921fba2912c9761600b2cc01b5c
edwin <emanuel@coboser.com>
2016-06-03 12:11:25 -0400

    edwin: correccion orden de rutas autos

--- app/Http/routes/certificateAuto.php
+++ app/Http/routes/certificateAuto.php
@@ -1,6 +1,6 @@
 <?php
 
-/**/
+# Cotizacion
 Route::group([ 'prefix' => 'au/{rp_id}/create/{header_id}/' ], function () {
     Route::post('slip_au', [
         'as' => 'au.slip_au',
@@ -8,22 +8,21 @@ Route::group([ 'prefix' => 'au/{rp_id}/create/{header_id}/' ], function () {
     ]);
 });
 
-Route::group([ 'prefix' => 'au/{rp_id}/issuance/{header_id}/' ], function () {
+# Emision
+Route::group([ 'prefix' => 'au/{rp_id}/issuance/{header_id}/'], function () {
     Route::post('slip_issuance', [
         'as' => 'slip_issuance',
         'uses' => 'slipModalAutoController@ajaxBuscar'
     ]);
-    });
+});
 
-    
+# Reportes emision - cotizacion
+Route::post('report/auto/general/slip/{id_comp}', [
+    'as' => 'slip_rep_au_general',
+    'uses' => 'slipModalAutoController@ajaxBuscar'
+]);
 
-    Route::post('report/auto/general/slip/{id_comp}', [
-        'as' => 'slip_rep_au_general',
-        'uses' => 'slipModalAutoController@ajaxBuscar'
-    ]);
-
-
-    Route::post('report/auto/cotizacion/slip/{id_comp}', [
-        'as' => 'slip_au_cot',
-        'uses' => 'slipModalAutoController@ajaxBuscar'
-    ]);
+Route::post('report/auto/cotizacion/slip/{id_comp}', [
+    'as' => 'slip_au_cot',
+    'uses' => 'slipModalAutoController@ajaxBuscar'
+]);
